12d66cd08e1991a72ea0acb4606651f639b83527
RavijaDul <134823036+RavijaDul@users.noreply.github.com>
2024-10-04 04:46:41 +0530

    publishKey

--- src/layouts/rootLayout/RootLayout.jsx
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -1,24 +1,48 @@
 import { Link,Outlet } from 'react-router-dom';
 import './rootLayout.css';
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+console.log('PRE_PUBLISHABLE_KEY_TEST:' );
+console.log(import.meta.env);  // This will log all available env variables
 
+//Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+//import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+console.log('PUBLISHABLE_KEY_TEST:' );
+console.log('PUBLISHABLE_KEY:',PUBLISHABLE_KEY );
+
+/*if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+  console.log('PUBLISHABLE_KEY:');
+  
+
+}*/
 const RootLayout = () => {
     return(
-        <div className='rootLayout'>
-            <header>
-                <Link to ="/"  className='logo'>
-
-                    <img src="/logo.png" alt="" />
-                    <span>NEOGPT</span>
-                </Link>
-                <div className="user">User</div>
-            </header>
-            <main>
-                <Outlet/>  
-            </main>
-        </div>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+            <div className='rootLayout'>
+                <header>
+                    <Link to ="/"  className='logo'>
+                        <img src="/logo.png" alt="" />
+                        <span>NEOGPT</span>
+                    </Link>
+                    <div className="user">
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                    </div>
+                </header>
+                <main>
+                    <Outlet/>
+                </main>
+            </div>
+        </ClerkProvider>
     )
 }
 
 export default RootLayout
 
 
+//<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/"> </ClerkProvider>
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import ChatPage from './routes/chatPage/ChatPage';
 import DashboardPage from './routes/dashboardPage/DashboardPage';
 import RootLayout from './layouts/rootLayout/RootLayout';
 import DashboardLayout from './layouts/dashboardLayout/DashboardLayout';
+import { ClerkProvider } from '@clerk/clerk-react';
+
 
 
 const router = createBrowserRouter([
@@ -28,7 +30,15 @@ const router = createBrowserRouter([
   },
 ]);
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+      <RouterProvider router={router} />
+    
+  </React.StrictMode>
+);
 
+/*
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
